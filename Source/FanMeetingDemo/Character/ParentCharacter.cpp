6fda1c26d564c2f7135aec286d0f1d314583abac
imusun <imusun190812@gmail.com>
2022-09-25 02:27:26 +0900

    #10. HandShake (PC) 추가

--- Source/FanMeetingDemo/Character/ParentCharacter.cpp
+++ Source/FanMeetingDemo/Character/ParentCharacter.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "ParentCharacter.h"
 #include "../UI/NamePlate.h"
 
 #include "UniversalVoiceChatPro/Public/PlayerVoiceChatActor.h"
 
 #include "Net/UnrealNetwork.h"
-#include "ParentCharacter.h"
 
 AParentCharacter::AParentCharacter()
 {
